44e56714824e3b9ba33cfb2e1d28d1cd61290327
Arjun A <akarjun313@gmail.com>
2024-08-23 21:22:34 +0530

    some review edits

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,6 +2,7 @@ import { findUser } from "../logic/basic-logic/findUser.js"
 import Bookings from "../models/bookingModel.js"
 import Product from "../models/productModel.js"
 import Review from "../models/reviewModel.js"
+import User from "../models/userModel.js"
 
 // FOR USERS 
 
@@ -15,7 +16,7 @@ export const addReview = async (req, res) => {
 
         //getting booking details
         const bookingDetails = await Bookings.findById(id)
-        if(!bookingDetails) return res.json({ message: 'Booking not found', success: false })
+        if (!bookingDetails) return res.json({ message: 'Booking not found', success: false })
 
 
         //check if user already added review
@@ -25,7 +26,7 @@ export const addReview = async (req, res) => {
         }
         console.log(findReview)
 
-        if(bookingDetails.status !== 'Delivered'){
+        if (bookingDetails.status !== 'Delivered') {
             return res.json({ message: 'Product must be delivered first', success: false })
         }
 
@@ -39,7 +40,7 @@ export const addReview = async (req, res) => {
 
         //save review
         const saveReview = await newReview.save()
-        if(!saveReview) return res.json({ message: "Failed, error in adding review", success: false })
+        if (!saveReview) return res.json({ message: "Failed, error in adding review", success: false })
 
 
         // Update product rating
@@ -50,7 +51,7 @@ export const addReview = async (req, res) => {
         await Product.findByIdAndUpdate(bookingDetails.product, { rating: averageRating })
 
         return res.status(200).json({ message: 'Review added successfully', success: true })
-        
+
     } catch (error) {
         console.log('Error in adding new review', error)
         res.status(500).json({ message: 'Error in adding new review', success: false })
@@ -61,27 +62,41 @@ export const addReview = async (req, res) => {
 export const showReviews = async (req, res) => {
     try {
         //Product id
-        const {id} = req.params
+        const { id } = req.params
         console.log("Product id :", id)
 
         //fetching reviews
         const showReviews = await Review.find({ product: id })
-        if(showReviews.length === 0) return res.json({ message: "No reviews found", success: false })
+        if (showReviews.length === 0) return res.json({ message: "No reviews found", success: false })
 
-        return res.status(200).json({ message: showReviews, success: true })
+        const reviewWithUserDetails = await Promise.all(showReviews.map(async (review) => {
+            const user = await User.findById(review.user); // Find user details
+            return {
+                ...review._doc,
+                user: {
+                    id: user._id,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    email: user.email,
+                    // Add other user details you want to include
+                }
+            }
+        }))
+
+        return res.status(200).json({ message: reviewWithUserDetails, success: true })
 
     } catch (error) {
         console.log('Error in displaying reviews', error)
         res.status(500).json({ message: 'Error in displaying reviews', success: false })
     }
-} 
+}
 
 
 // delete review
 export const deleteReview = async (req, res) => {
     try {
         //review id
-        const {id} = req.params
+        const { id } = req.params
 
         //find user
         let user = ''
@@ -94,17 +109,17 @@ export const deleteReview = async (req, res) => {
 
         //find review
         const findReview = await Review.findById(id)
-        if(!findReview) return res.status(400).json({ message: 'Review not found', success: false })
+        if (!findReview) return res.status(400).json({ message: 'Review not found', success: false })
 
         //check if review belongs to user
-        if(findReview.user !== user._id) return res.status(400).json({ message: 'Only review owner can delete the review', success: false })
+        if (findReview.user !== user._id) return res.status(400).json({ message: 'Only review owner can delete the review', success: false })
 
         //delete review
         const deleteReview = await Review.findByIdAndDelete(id)
-        if(!deleteReview) return res.status(400).json({ message: 'Error in deleting review', success: false })
-        
+        if (!deleteReview) return res.status(400).json({ message: 'Error in deleting review', success: false })
+
         return res.status(200).json({ message: 'Review deleted', success: true })
-        
+
     } catch (error) {
         console.log('Error in deleting review', error)
         res.status(500).json({ message: 'Error in deleting review', success: false })
